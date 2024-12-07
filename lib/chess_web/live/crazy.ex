a0f392d385992e56bc97aa84454a446eed96f308
hmb1091 <hmb1091@usnh.edu>
2024-12-06 20:26:27 -0500

    Update crazy.ex
    added invite link

--- lib/chess_web/live/crazy.ex
+++ lib/chess_web/live/crazy.ex
@@ -17,6 +17,10 @@ defmodule ChessWeb.Live.CrazyChess do
 	Chess.PubSub.subscribe("#{@pubsub_topic_prefix}#{id}")
 	
 	Logger.info("Found existing game (#{id})")
+
+	# Generate the invite link for the game
+      	invite_link = Routes.live_path(socket, ChessWeb.Live.CrazyChess, id)
+
 	{:ok, socket |> assign(:game, id)
                      |> assign(:board, game_state.board)
                      |> assign(:turn, :black)
